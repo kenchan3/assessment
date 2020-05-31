41fe8e67bde177e2f1b247bab729f1a92d937a2c
kenchan3 <66044143+kenchan3@users.noreply.github.com>
2020-06-01 08:29:20 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -59,7 +59,7 @@ userNameInput.onkeydowmn = event => {
     }
 };
 
-const answers = [,
+const answers = [
     '{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。',
     '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
     '{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。',
